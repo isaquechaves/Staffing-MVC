6771089c1dc91fa4a376d93e779c7f42f9e57b84
ieca <isaque.oliveira@gft.com>
2020-11-03 15:43:09 -0300

    DummyData funcionario e vaga

--- src/main/java/br/com/gft/staffing/model/Vaga.java
+++ src/main/java/br/com/gft/staffing/model/Vaga.java
@@ -30,7 +30,56 @@ public class Vaga {
 	
 	@NotBlank
 	private String projeto;
-	
-	@NotBlank
+		
 	private int qtd_vaga;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public LocalDate getAbertura_vaga() {
+		return abertura_vaga;
+	}
+
+	public void setAbertura_vaga(LocalDate abertura_vaga) {
+		this.abertura_vaga = abertura_vaga;
+	}
+
+	public String getCodigo_vaga() {
+		return codigo_vaga;
+	}
+
+	public void setCodigo_vaga(String codigo_vaga) {
+		this.codigo_vaga = codigo_vaga;
+	}
+
+	public String getDescricao_vaga() {
+		return descricao_vaga;
+	}
+
+	public void setDescricao_vaga(String descricao_vaga) {
+		this.descricao_vaga = descricao_vaga;
+	}
+
+	public String getProjeto() {
+		return projeto;
+	}
+
+	public void setProjeto(String projeto) {
+		this.projeto = projeto;
+	}
+
+	public int getQtd_vaga() {
+		return qtd_vaga;
+	}
+
+	public void setQtd_vaga(int qtd_vaga) {
+		this.qtd_vaga = qtd_vaga;
+	}
+	
+	
 }
--- src/main/java/br/com/gft/staffing/utils/DummyData.java
+++ src/main/java/br/com/gft/staffing/utils/DummyData.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.gft.staffing.model.Funcionario;
+import br.com.gft.staffing.model.Vaga;
 import br.com.gft.staffing.repository.FuncionarioRepository;
+import br.com.gft.staffing.repository.VagaRepository;
 
 
 @Component
@@ -18,6 +20,9 @@ public class DummyData {
 	
 	@Autowired
 	FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	VagaRepository vagaRepository;
 	
 	@PostConstruct	
 	public void saveFuncionario(){
@@ -27,7 +32,7 @@ public class DummyData {
 		funcionario1.setCargo("Desenvolvedor Back-end");
 		funcionario1.setInicio_wa(LocalDate.now());
 		funcionario1.setTermino_wa(LocalDate.now().plusDays(15));
-		funcionario1.setNome("João Pedro");
+		funcionario1.setNome("João Pedro");		
 		
 		
 		Funcionario funcionario2 = new Funcionario();
@@ -51,7 +56,35 @@ public class DummyData {
 		for(Funcionario funcionario : funcionarioList) {
 			Funcionario funcionarioSaved = funcionarioRepository.save(funcionario);
 			System.out.println(funcionarioSaved.getId());
-		}
+		}	
+	}
 	
+	@PostConstruct
+	public void saveVaga() {
+		List<Vaga> vagaList = new ArrayList<>();
+		
+		Vaga vaga1 = new Vaga();		
+		vaga1.setAbertura_vaga(LocalDate.now());
+		vaga1.setCodigo_vaga("Itau_12");
+		vaga1.setDescricao_vaga("Desenvolvedor back-end Java");
+		vaga1.setProjeto("Itaú pix");
+		vaga1.setQtd_vaga(1);
+		
+		Vaga vaga2 = new Vaga();
+		vaga2.setAbertura_vaga(LocalDate.now());
+		vaga2.setCodigo_vaga("Itau_12");
+		vaga2.setDescricao_vaga("Desenvolvedor back-end Java");
+		vaga2.setProjeto("Itaú pix");
+		vaga2.setQtd_vaga(2);
+		
+		vagaList.add(vaga1);
+		vagaList.add(vaga2);
+		
+		for(Vaga vaga : vagaList) {
+			Vaga vagaSaved = vagaRepository.save(vaga);
+			System.out.println(vagaSaved.getId());
+		}	
+		
+		
 	}
 }

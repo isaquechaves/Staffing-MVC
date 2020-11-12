719daa15ba98a41f47e7cc1038969da1fd92c6c3
Isaque <isaque.oliveira2@fatec.sp.gov.br>
2020-11-12 16:13:16 -0300

    Staffing MVC
    Staffing MVC

---
+++ target/classes/static/js/staffing.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+  
+
+  $('a[name=vaga]').click(function() {
+    
+   	var vaga_id = document.getElementById("vaga_id");
+    console.log(vaga_id);
+    
+  });
+
+});	
